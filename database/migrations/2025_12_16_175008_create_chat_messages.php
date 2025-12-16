10b6e6cd4fe0d234cd72c2f4122b315a85a5adab
Satyasy <satyavpandega@gmail.com>
2025-12-17 01:28:28 +0700

    Create Migrations Database

---
+++ database/migrations/2025_12_16_175008_create_chat_messages.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chat_messages', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('session_id');
+            $table->foreign('chat_sessions')->references('id')->on('chat_sessions')->onDelete('cascade');
+            $table->enum('sender', ['user', 'ai']);
+            $table->longText('message');
+            $table->json('structured_response')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chat_messages');
+    }
+};
